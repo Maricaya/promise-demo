750f38b48f5077d6f50e1541e7f4fb26a61e7ef6
Maricaya <915270549@qq.com>
2020-01-10 17:06:05 +0800

    feat: 完成一个A+的promise

---
+++ README.md
@@ -0,0 +1,15 @@
+## promise
+
+## 测试
+引入chai和sinon进行测试。启动测试：
+```js
+yarn test
+```
+
+## 使用步骤
+- 点击 Fork
+- 完善 promise-test 里的 src/promist.ts 代码
+- 上传代码，点击你的仓库页面的 create pull request 按钮
+- Travis CI 会检查你的代码是否正确
+- 如果测试成功，你会看到绿色的成功提示
+- 如果测试失败，你会看到失败提示
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+Promise.resolve('{"name":"cxl"}')
+    .then(s => {
+        console.log(JSON.parse(s))
+    })
+
+class Promise2 {
+    state = 'pending'
+    callbacks = []
+
+    resolve(result) {
+        if (this.state !== 'pending') return
+        this.state = 'fulfilled'
+        setTimeout(() => {
+            // 遍历callbacks，调用所有的handle[0]
+            this.callbacks.forEach(handle => {
+                if (typeof handle[0] === 'function') {
+                    handle[0].call(undefined, result)
+                }
+            })
+        }, 0)
+    }
+    reject(reason) {
+        if (this.state !== 'pending') return
+        this.state = 'rejected'
+        setTimeout(() => {
+            this.callbacks.forEach(handle => {
+                if (typeof handle[1] === 'function') {
+                    handle[1].call(undefined, reason)
+                }
+            })
+        }, 0)
+    }
+    constructor(fn) {
+        if (typeof fn !== 'function') {
+            throw new Error('我只接受函数')
+        }
+        console.log(this)
+        fn(this.resolve.bind(this), this.reject.bind(this))
+    }
+    then(succeed?, fail?) {
+        const handle = []
+        if (typeof succeed === 'function') {
+            handle[0] = succeed
+        }
+        if (typeof fail === 'function') {
+            handle[1] = fail
+        }
+        // 把函数推进callbacks
+        this.callbacks.push(handle)
+        return  
+    }
+}
+
+export default Promise2
